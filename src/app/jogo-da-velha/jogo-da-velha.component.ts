7e2b7c79727b3ed61bde6ca2cbf0497efe17d537
Axel Georg <axelgeorg16@gmail.com>
2020-05-22 15:07:22 -0300

    Jogo da velha

---
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { JogoDaVelhaService } from './shared';
+
+@Component({
+  selector: 'app-jogo-da-velha', //tag
+  templateUrl: './jogo-da-velha.component.html',
+  styleUrls: ['./jogo-da-velha.component.css']
+})
+export class JogoDaVelhaComponent implements OnInit {
+
+  constructor(private jogoDaVelhaService: JogoDaVelhaService) { }
+
+  ngOnInit(): void {
+    this.jogoDaVelhaService.inicializar();
+  }
+
+}
