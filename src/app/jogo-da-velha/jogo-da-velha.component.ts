4ca03dd96831d5f440443075ce12017ae0411183
Axel Georg <axelgeorg16@gmail.com>
2020-05-24 19:17:18 -0300

    implementando a tela do tabuleiro do jogo da velha - 1.0

--- src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -59,7 +59,57 @@ export class JogoDaVelhaComponent implements OnInit {
    * @return void
    */
   jogar(posX: number, posY: number): void {
-    
+    this.jogoDaVelhaService.jogar(posX, posY);
   }
 
+  /**
+   * Retorna se a peça X deve ser exibida para coordenada informada.
+   *
+   * @param number posX
+   * @param number posY
+   * @return boolean
+   */
+  exibirX(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirX(posX, posY);
+  }
+
+  /**
+   * Retorna se a peça O deve ser exibida para coordenada informada.
+   *
+   * @param number posX
+   * @param number posY
+   * @return boolean
+   */
+  exibirO(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirO(posX, posY);
+  }
+
+  /**
+   * Retorna se a marcação de vitória deve ser exibida para a coordenada informada.
+   *
+   * @param number posX
+   * @param number posY
+   * @return boolean
+   */
+  exibirVitoria(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirVitoria(posX, posY);
+  }
+
+  /**
+   * Retorna o número do jogador a jogar.
+   *
+   * @return number
+   */
+  get jogador(): number {
+    return this.jogoDaVelhaService.jogador;
+  }
+
+  /**
+   * Inicia um novo jogo.
+   *
+   * @return void
+   */
+  novoJogo(): void {
+    this.jogoDaVelhaService.i
+  }
 }
