f00d1be189b356d10c0418649f05863ed076212c
Axel Georg <axelgeorg16@gmail.com>
2020-05-24 10:41:22 -0300

    Implementando a tela inicial do jogo da velha

--- src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -15,4 +15,51 @@ export class JogoDaVelhaComponent implements OnInit {
     this.jogoDaVelhaService.inicializar();
   }
 
+  /**
+   * Retorna se o tabuleiro deve ser exibido
+   *
+   * @return boolean
+   */
+  get showTabuleiro(): boolean {
+    return this.jogoDaVelhaService.showTabuleiro;
+  }
+
+  /**
+   * Retorna se a tela de inicio deve ser exibido
+   *
+   * @return boolean
+   */
+  get showInicio(): boolean {
+    return this.jogoDaVelhaService.showInicio;
+  }
+
+  /**
+   * Retorna se a tela de fim de jogo deve ser exibido
+   *
+   * @return boolean
+   */
+  get showFinal(): boolean {
+    return this.jogoDaVelhaService.showFinal;
+  }
+
+  /**
+   * Inicializa os dados de um novo jogo
+   *
+   * @return void
+   */
+  iniciarJogo(): void {
+    this.jogoDaVelhaService.iniciarJogo();
+  }
+
+  /**
+   * Realiza uma jogada ao clicar num local do tabuleiro
+   *
+   * @param number posX
+   * @param number posY
+   * @return void
+   */
+  jogar(posX: number, posY: number): void {
+    
+  }
+
 }
